2100e24abbabe56e7bc8076e7a516607d4400844
cbuzzz <sebpinsan12@gmail.com>
2024-11-01 17:53:01 +0100

    Entrega ejercicio JWT

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import { getUsers, createUser, getUser, updateUser, deleteUser, login, profile } from '../controllers/userController';
-import { TokenValidation} from '../middleware/verifyJWT'
+import { TokenValidation } from '../middleware/verifyJWT'
 import { verifyOwnership } from '../middleware/verifyOwner'
-import { AdminValidation} from '../middleware/verifyAdmin'
+import { AdminValidation } from '../middleware/verifyAdmin'
 const router = express.Router();
 
 // Ruta para obtener todos los usuarios
-router.get("/", getUsers);
+// router.get("/", getUsers);
+
+//Ruta para que solo los administradores 
+//puedan obtener todos los usuarios
+router.get("/", TokenValidation, AdminValidation, getUsers);
 
 // Ruta per crear usuari
 router.post("/", createUser);
@@ -14,8 +18,12 @@ router.post("/", createUser);
 //Ruta per obtenir usuari per id
 router.get("/:id", getUser);
 
-//Ruta per actialitzar usuari per id
-router.put("/update/:id", updateUser);
+//Ruta per actualitzar usuari per id
+// router.put("/update/:id", updateUser);
+
+//Ruta para que solo el owner o un admin pueda
+//actualizar un usuario por id
+router.put("/update/:id", TokenValidation, verifyOwnership, updateUser);
 
 //Ruta per eliminar user per id
 router.delete('/delete/:id', TokenValidation, AdminValidation, deleteUser);
